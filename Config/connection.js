7b1e21458f0bbbba9959dc69fb1f846a50d8b117
dilshad <md.mdsar@gmail.com>
2024-02-01 22:42:29 +0530

    config updated

--- Config/connection.js
+++ Config/connection.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const ConnectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB);
+    await mongoose.connect(process.env.MONGODB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Db Connected");
   } catch (error) {
     console.error(error, "Db error");

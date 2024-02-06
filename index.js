58b004f57f5b027841aa32480af9eef756046511
dilshad <md.mdsar@gmail.com>
2024-02-06 20:51:29 +0530

    index file updated

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import bodyParser from "body-parser";
-
-const app = express();
-
 import { ConnectDB } from "./Config/connection.js";
 import userRoutes from "./Routes/UserRoutes.js";
 
-// Middleware
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
 app.use(cors());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
 ConnectDB();
 
-const port = process.env.PORT;
+// Routes
+app.use("/auth", userRoutes);
 
 app.listen(port, () => {
-  console.log("started");
+  console.log(`Server is running on port ${port}`);
 });
-
-app.use("/auth", userRoutes);
